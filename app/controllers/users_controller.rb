4f5676dc8d25e35fcdce2d2b6dfc7aabf0824359
Eve Waller <evew1991@outlook.com>
2025-01-29 08:48:35 +0000

    added username to user table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to @user, notice: "Profile updated successfully"
+    else
+      flash.now[:alert] = "There was a problem updating your profile."
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
