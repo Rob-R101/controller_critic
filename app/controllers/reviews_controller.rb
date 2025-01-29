689715407b8b7cc575f488e9f18802df33107073
rob roffey <roffeyre@gmail.com>
2025-01-29 12:55:15 +0000

    user leave review added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @game = Game.find(params[:game_id])
     @reviews = @game.reviews
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @review = @game.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to game_path(@game), notice: 'Review added successfully.'
+    else
+      redirect_to game_path(@game), alert: 'Unable to add review.'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:review)
   end
 end
