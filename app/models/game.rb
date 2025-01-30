525a033693ab29118b8b7abce4fd75392cbf9f7f
Eve Waller <evew1991@outlook.com>
2025-01-30 17:03:43 +0000

    logic fixed for view page and models edited

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,11 @@
 class Game < ApplicationRecord
   has_many :reviews
   has_many :my_games
-  has_many :users, through: :my_games
+  has_many :player_users, through: :my_games, source: :user
+
   has_many :wishlists
-  has_many :users, through: :wishlists
+  has_many :wished_users, through: :wishlists, source: :user
+
   has_many :game_platforms
   has_many :platforms, through: :game_platforms
 end
